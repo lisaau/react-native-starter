3cf8bb4456a4a792a513c7205a34931b4aee5785
lisaau <lisaau@users.noreply.github.com>
2020-04-09 16:03:16 -0700

    Add counter screen to prep for adding state

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -17,6 +17,10 @@ const HomeScreen = ({ navigation }) => {
       onPress={() => navigation.navigate('Image')}
       title="Go to Image Demo" 
     />
+    <Button 
+      onPress={() => navigation.navigate('Counter')}
+      title="Go to Counter Demo" 
+    />
     {/* <TouchableOpacity onPress={() => navigation.navigate('List')}>
       <Text>Go to List Demo</Text>
     </TouchableOpacity> */}
