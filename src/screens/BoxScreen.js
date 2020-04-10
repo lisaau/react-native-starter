5fcf6899da9ef0f1d7ee5bf68c53fb902e68d532
lisaau <lisaau@users.noreply.github.com>
2020-04-10 13:33:40 -0700

    Add flexbox positioning

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -2,21 +2,51 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 const BoxScreen = () => {
-    return <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>BoxScreen</Text>
+    return <View>
+        <Text>Parent: alignItems, justifyContent, flexDirection</Text>
+        <Text>Child: flex, alignSelf</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.textOneStyle}>Child # 1</Text>
+            <Text style={styles.textStyle}>Child # 2</Text>
+            <Text style={styles.textStyle}>Child # 3</Text>
+        </View>
+        <View style={styles.viewStyleColumn}>
+            <Text style={styles.textOneStyle}>Child # 4</Text>
+            <Text style={styles.textTwoStyle}>Child # 5</Text>
+            <Text style={styles.textStyle}>Child # 6</Text>
+        </View>
     </View>
 }
 
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
-        borderColor: 'black'
+        borderColor: 'black',
+        alignItems: 'center',
+        flexDirection: 'row',
+        height: 200,
+    },
+    viewStyleColumn: {
+        borderWidth: 3,
+        borderColor: 'blue',
+        justifyContent: 'center',
+        height: 200,
     },
     textStyle: {
         borderWidth: 3,
         borderColor: 'red',
         margin: 20,
 
+    },
+    textOneStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        flex: 1
+    },
+    textTwoStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        alignSelf: 'flex-end'
     }
 });
 
