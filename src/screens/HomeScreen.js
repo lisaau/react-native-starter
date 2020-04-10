d4629f5707c6b7037a4efa77a281c8bb33ad0417
lisaau <lisaau@users.noreply.github.com>
2020-04-10 10:06:24 -0700

    Add random color

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -21,6 +21,10 @@ const HomeScreen = ({ navigation }) => {
       onPress={() => navigation.navigate('Counter')}
       title="Go to Counter Demo" 
     />
+    <Button 
+      onPress={() => navigation.navigate('Color')}
+      title="Go to Color Demo" 
+    />
     {/* <TouchableOpacity onPress={() => navigation.navigate('List')}>
       <Text>Go to List Demo</Text>
     </TouchableOpacity> */}
