955ffc51d19438a765c1303d43908b6b17dd358e
lisaau <lisaau@users.noreply.github.com>
2020-04-10 12:46:30 -0700

    Add styling to demonstate box object model

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 const BoxScreen = () => {
-    return <View>
-        <Text>BoxScreen</Text>
+    return <View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>BoxScreen</Text>
     </View>
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: 'black'
+    },
+    textStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        margin: 20,
+
+    }
+});
 
 export default BoxScreen
